ca009707a1c9104da3dd5161384b6b16818e5bf0
Starlog <785276471@qq.com>
2020-11-15 20:14:17 +0800

    添加350两个数组的交集II

--- 算法/快速排序.js
+++ 算法/快速排序.js
@@ -2,15 +2,15 @@ function quickSort(arr) {
   let left_arr = [];
   let right_arr = [];
   let middle = arr[0];
-  for(let i=1;i<arr.length;i++) {
-    if(arr[i] < middle) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < middle) {
       left_arr.push(arr[i])
     } else {
       right_arr.push(arr[i])
     }
   }
-  if(left_arr.length >= 2) left_arr = quickSort(left_arr);
-  if(right_arr.length >= 2) right_arr = quickSort(right_arr);
+  if (left_arr.length >= 2) left_arr = quickSort(left_arr);
+  if (right_arr.length >= 2) right_arr = quickSort(right_arr);
 
   return left_arr.concat(middle, right_arr)
 }
