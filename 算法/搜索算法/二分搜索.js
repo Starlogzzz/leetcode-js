8542dd685056589eea6130f762db24982b5e81f1
Starlog <785276471@qq.com>
2020-12-07 16:53:55 +0800

    添加搜索算法

---
+++ 算法/搜索算法/二分搜索.js
@@ -0,0 +1,21 @@
+// 时间复杂度O(logn)
+function search(item) {
+  let low = 0;
+  let high = this.length - 1;
+  while(low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const element = this[mid];
+    if(element < item) {
+      low = mid + 1;
+    } else if(element > item) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+Array.prototype.search = search;
+
+let arr = [1,2,3,4,5,6];
+console.log(arr.search(2))
